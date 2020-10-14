5c4cc4294e2743b7beec5a28523dc1dc8b850add
qiaolijie <1213267233@qq.com>
2020-10-14 09:30:36 +0800

    关联

---
+++ src/config.js
@@ -0,0 +1,10 @@
+var debug = true;
+
+module.exports = {
+  apibase: debug ? 'http://192.168.31.200:8080/' : 'http://api.pro.com/',
+  themeColor: {
+    topBarColor: '#0273b1',
+    topBarFontColor: '#fff'
+  }
+};
+
